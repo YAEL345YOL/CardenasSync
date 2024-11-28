e60b7a81dbdb85393c5060a6be807f3f171fd9cb
YAEL345YOL <lenin346yael@gmail.com>
2024-11-27 20:31:36 -0600

    idk

--- agregar_trabajador.php
+++ agregar_trabajador.php
@@ -4,15 +4,15 @@ include "coneccion.php";
 $con = coneccion();
 
 $id = null;
-$nombre = $_POST["nombre_trabajdor"];
+$nombre = $_POST["nombre_trabajador"];
 $apellido = $_POST["apellido_trabajador"];
 $puesto_trabajo = $_POST["puesto_trabajador"];
 $fecha_nacimiento = $_POST["fecha_nacimiento_trabajador"];
 $telefono = $_POST["telefono_trabajador"];
-$correo_electronico = $_POST["correo_eletronico_trabajador"];
+$correo_electronico = $_POST["correo_electronico_trabajador"];
 $contrasena_trabajador = "changeme";
 
-$sql = "INSERT INTO trabajador VALUES('$id','$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo_electronico','$contrasena_trabajador')";
+$sql = "INSERT INTO trabajador VALUES ('$id','$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo_electronico','$contrasena_trabajador')";
 mysqli_query($con,$sql);
 
 ?>
