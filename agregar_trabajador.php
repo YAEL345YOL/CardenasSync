3d80616f0243611c7334d012bf57473a500321ea
YAEL345YOL <lenin346yael@gmail.com>
2024-11-27 20:26:28 -0600

    xd

--- agregar_trabajador.php
+++ agregar_trabajador.php
@@ -10,8 +10,9 @@ $puesto_trabajo = $_POST["puesto_trabajador"];
 $fecha_nacimiento = $_POST["fecha_nacimiento_trabajador"];
 $telefono = $_POST["telefono_trabajador"];
 $correo_electronico = $_POST["correo_eletronico_trabajador"];
+$contrasena_trabajador = "changeme";
 
-$sql = "INSERT INTO trabajador VALUES('$id','$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo_electronico')";
+$sql = "INSERT INTO trabajador VALUES('$id','$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo_electronico','$contrasena_trabajador')";
 mysqli_query($con,$sql);
 
 ?>
