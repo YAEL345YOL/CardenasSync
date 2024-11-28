ca208e7eb7096a9bc087e9e94556b0d49cdc875a
YAEL345YOL <lenin346yael@gmail.com>
2024-11-27 20:40:30 -0600

    Ya jala agregar trabajador xd

--- agregar_trabajador.php
+++ agregar_trabajador.php
@@ -2,17 +2,15 @@
 include "coneccion.php";
 
 $con = coneccion();
-
-$id = null;
 $nombre = $_POST["nombre_trabajador"];
 $apellido = $_POST["apellido_trabajador"];
-$puesto_trabajo = $_POST["puesto_trabajador"];
+$puesto_trabajo = $_POST["puesto_trabajo_trabajador"];
 $fecha_nacimiento = $_POST["fecha_nacimiento_trabajador"];
 $telefono = $_POST["telefono_trabajador"];
 $correo = $_POST["correo_electronico_trabajador"];
 $contrasena= "changeme";
 
-$sql = "INSERT INTO `trabajador` (`id_trabajador`, `nombre_trabajador`, `apellido_trabajador`, `puesto_trabajador`, `fechanacimiento_trabajador`, `telefono_trabajador`, `correo_trabajador`, `contrasena_trabajador`) VALUES ('$id','$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo','$contrasena');";
+$sql = "INSERT INTO `trabajador` (`id_trabajador`, `nombre_trabajador`, `apellido_trabajador`, `puesto_trabajador`, `fechanacimiento_trabajador`, `telefono_trabajador`, `correo_trabajador`, `contrasena_trabajador`) VALUES (NULL,'$nombre','$apellido','$puesto_trabajo','$fecha_nacimiento','$telefono','$correo','$contrasena');";
 mysqli_query($con,$sql);
 
 ?>
