fc2319e1f3dd2027f40053dccf86bbe2a9b15ca9
YAEL345YOL <lenin346yael@gmail.com>
2024-12-03 18:39:38 -0600

    Cambios peque;os

--- views/index.php
+++ views/index.php
@@ -8,6 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/styles.css">
     <title>CardenasSync</title>
 </head>
 <body>
